05d7b1d028bf885c2a402db64e50aa1c5abc3094
Muhamad Mupid Ahmadiawan <133787157+Yuefii@users.noreply.github.com>
2024-08-03 14:49:48 +0700

    refactor: add comments to repository

--- internal/domain/repositories/user_repository.go
+++ internal/domain/repositories/user_repository.go
@@ -6,24 +6,32 @@ import (
 	"ngodeyuk-core/internal/domain/models"
 )
 
+// untuk mendefinisikan metode yang diperlukan untuk berinteraksi dengan repository user
 type UserRepository interface {
+	// menyimpan user baru ke database
 	Create(user *models.User) error
+	// mencari user berdasarkan username
 	FindByUsername(username string) (*models.User, error)
+	// memperbarui data user yang ada di database
 	Update(user *models.User) error
 }
 
+// untuk mengimplementasikan repository user mengunakan gorm
 type userRepository struct {
 	db *gorm.DB
 }
 
+// untuk membuat instance baru dengan menginisialisasi koneksi database
 func NewUserRepository(db *gorm.DB) UserRepository {
 	return &userRepository{db}
 }
 
+// untuk menyimpan user baru kedalam database menggunakan gorm
 func (repository *userRepository) Create(user *models.User) error {
 	return repository.db.Create(user).Error
 }
 
+// untuk mencari user berdasarkan username, lalu mengembalikan user jika ditemukan dan error jika tidak ditemukan
 func (repository *userRepository) FindByUsername(username string) (*models.User, error) {
 	var user models.User
 	err := repository.db.Where("username = ?", username).First(&user).Error
@@ -33,6 +41,7 @@ func (repository *userRepository) FindByUsername(username string) (*models.User,
 	return &user, nil
 }
 
+// untuk memperbarui data user yang sudah ada didatabase menggunakan gorm
 func (repository *userRepository) Update(user *models.User) error {
 	if err := repository.db.Save(user).Error; err != nil {
 		return err
