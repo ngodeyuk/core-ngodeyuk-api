ad307d8021f27e53c7223b197af16ed37a39e874
Muhamad Mupid Ahmadiawan <133787157+Yuefii@users.noreply.github.com>
2024-08-03 14:50:49 +0700

    refactor: add comments to repository (#19)

--- internal/domain/repositories/course_repository.go
+++ internal/domain/repositories/course_repository.go
@@ -6,18 +6,23 @@ import (
 	"ngodeyuk-core/internal/domain/models"
 )
 
+// untuk mendefinisikan metode yang diperlukan untuk berinteraksi dengan repository course
 type CourseRepository interface {
+	// menyimpan data course baru ke database
 	Create(course *models.Course) error
 }
 
+// untuk mengimplementasikan repository course menggunakan gorm
 type courseRepository struct {
 	db *gorm.DB
 }
 
+// untuk membuat instance baru dengan menginisialisasi koneksi database
 func NewCourseRepository(db *gorm.DB) CourseRepository {
 	return &courseRepository{db}
 }
 
+// untuk menyimpan data course baru kedalam database menggunakan gorm
 func (repository *courseRepository) Create(course *models.Course) error {
 	return repository.db.Create(course).Error
 }
