02c8e2f8b9333a042464f40a463ef031ced25c74
Muhamad Mupid Ahmadiawan <133787157+Yuefii@users.noreply.github.com>
2024-08-08 18:18:49 +0700

    feat: add cors origin (#28)

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"path/filepath"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 
@@ -14,6 +15,13 @@ import (
 
 func main() {
 	route := gin.Default()
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"*"}
+	config.AllowMethods = []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}
+	config.AllowHeaders = []string{"Origin", "Content-Type"}
+
+	route.Use(cors.New(config))
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("error load .env file: %v", err)
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require github.com/gin-gonic/gin v1.10.0
 require github.com/golang-jwt/jwt/v5 v5.2.1
 
 require (
+	github.com/gin-contrib/cors v1.7.2 // indirect
 	github.com/kr/text v0.2.0 // indirect
 	github.com/rogpeppe/go-internal v1.12.0 // indirect
 )
