e861ff4c5726f1491b3e45d69fc63d8d212df4fc
Yuefii <muffidahmadiawan31@gmail.com>
2024-08-20 15:59:18 +0700

    fix: load env config

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,15 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
+	"ngodeyuk-core/database"
+	"ngodeyuk-core/internal/infrastructure/routes"
+	"ngodeyuk-core/pkg/utils"
 	"path/filepath"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
-
-	"ngodeyuk-core/database"
-	"ngodeyuk-core/internal/infrastructure/routes"
 )
+
+func init() {
+	utils.LoadEnv()
+}
 
 func main() {
 	route := gin.Default()
@@ -21,11 +24,6 @@ func main() {
 	config.AllowHeaders = []string{"Origin", "Content-Type"}
 
 	route.Use(cors.New(config))
-
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("error load .env file: %v", err)
-	}
 
 	db, err := database.InitDB()
 	if err != nil {
